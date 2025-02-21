dcfaff9faa72b276925a24ed5516589eb9649de8
Simon.J <896073330@qq.com>
2025-02-21 20:44:40 +0800

    更新 Locale

--- src/main.js
+++ src/main.js
@@ -1,20 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import i18n from './locales/i18n'
+import i18n, { useLocale } from './locales/i18n'
 import * as echarts from 'echarts'
 import 'vant/lib/index.css';
 import './style.css'
-import { Locale } from 'vant';
 import { createPinia } from 'pinia';
 
-// import Vant from 'vant';
-import enUS from 'vant/lib/locale/lang/en-US';
-Locale.use('en-US', enUS);
+useLocale();
+
 const app = createApp(App)
 app.config.globalProperties.$echarts = echarts
 app.use(router)
 app.use(i18n)
 app.use(createPinia())
-// app.use(Vant);
 app.mount('#app')
